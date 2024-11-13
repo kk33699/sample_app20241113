057db93abf27cbeb0e17545741102d88d8563770
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-11-13 13:44:22 +0000

    3-9_20241113_2[Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+
   get '/top' => 'homes#top'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists
 end
